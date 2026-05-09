68530e22fac000f9829d7373e1b674ca28a6a3dd
Harvin <jeffethramos@gmail.com>
2026-05-09 16:20:02 -0600

    renombrando a estandar los services con .service y configuraciones de componentes

--- inventory-app/src/app/components/dashboard/dashboard.ts
+++ inventory-app/src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, computed, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InventoryService } from '../../services/inventory.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { ProductHistory } from '../product-history/product-history';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatTableModule, MatButtonModule, ProductHistory],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss',
+  styleUrls: ['./dashboard.scss']
 })
-export class Dashboard {
+export class DashboardComponent implements OnInit {
+  private inventoryService = inject(InventoryService);
+  
+  // Signals de estado
+  selectedProductId = signal<number | null>(null);
 
+  // cargar datos de la API
+  products = this.inventoryService.products;
+  
+  // Signals de estado
+  totalProducts = computed(() => this.products().length);
+  criticalCount = computed(() => 
+    this.products().filter(p => p.currentStock <= p.minStock).length
+  );
+
+  ngOnInit() {
+    this.inventoryService.loadProducts();
+  }
+
+  selectProduct(id: number) {
+    this.selectedProductId.set(id);
+  }
 }
--- inventory-app/src/app/components/product-history/product-history.ts
+++ inventory-app/src/app/components/product-history/product-history.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component,input  } from '@angular/core';
 
 @Component({
   selector: 'app-product-history',
   imports: [],
+  standalone: true,
   templateUrl: './product-history.html',
   styleUrl: './product-history.scss',
 })
 export class ProductHistory {
-
+  //input de escucha para ver el producto historial
+ productId = input.required<number>(); 
 }
