0623bc0a4df1ac2e4dcb8e1628a51ce3c4be6d9c
Harvin <jeffethramos@gmail.com>
2026-05-09 16:02:10 -0600

    ajustes de services en comments

--- inventory-app/src/app/services/inventory.ts
+++ inventory-app/src/app/services/inventory.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Inventory {
+export class InventoryService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8080/api/v1';
+
   
+  products = signal<Product[]>([]);
+  loading = signal<boolean>(false);
+
+  async loadProducts() {
+    this.loading.set(true);
+    try {
+      const data = await firstValueFrom(this.http.get<Product[]>(`${this.apiUrl}/products`));
+      this.products.set(data);
+    } finally {
+      this.loading.set(false);
+    }
+  }
+
+  
+  async getAlerts() {
+    return firstValueFrom(this.http.get<Product[]>(`${this.apiUrl}/alerts`));
+  }
 }
+
