1825097c79863e71be0a95608a1dbe3e81a37362
Harvin <jeffethramos@gmail.com>
2026-05-09 15:54:49 -0600

    angular config initial

---
+++ inventory-app/src/app/components/product-history/product-history.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-history',
+  imports: [],
+  templateUrl: './product-history.html',
+  styleUrl: './product-history.scss',
+})
+export class ProductHistory {
+
+}
